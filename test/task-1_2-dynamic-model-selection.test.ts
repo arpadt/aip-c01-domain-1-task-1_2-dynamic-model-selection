b594800b06489802b65313180f772f9500e2f55d
Arpad Toth <a.arpad.toth@gmail.com>
2026-02-02 13:47:32 +0100

    Initial commit

---
+++ test/task-1_2-dynamic-model-selection.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Task12DynamicModelSelection from '../lib/task-1_2-dynamic-model-selection-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/task-1_2-dynamic-model-selection-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Task12DynamicModelSelection.Task12DynamicModelSelectionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
